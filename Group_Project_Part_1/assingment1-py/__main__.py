c814525e88d871f6a9116bf1861879f700a53886
cullenoi <cullenoi@localhost.localdomain>
2024-09-30 16:26:13 +0100

    Cormac is a flaker and my back sore from carrying

---
+++ Group_Project_Part_1/assingment1-py/__main__.py
@@ -0,0 +1,9 @@
+# Dafny program assingment1.dfy compiled into Python
+import sys
+from typing import Callable, Any, TypeVar, NamedTuple
+from math import floor
+from itertools import count
+
+import module_ as module_
+import _dafny as _dafny
+
