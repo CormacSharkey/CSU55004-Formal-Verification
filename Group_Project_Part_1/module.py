4f50d19801c4ee6f57edc71c6d6d85de5a0fe4a8
CormacSharkey <csharkey@tcd.ie>
2024-09-30 21:22:46 +0100

    creating a python module for strings1 file (handmade code in Python)

--- Group_Project_Part_1/module.py
+++ Group_Project_Part_1/module.py
@@ -18,3 +18,31 @@ def isSubstring(sub, str):
                 return True
     return False
 
+def haveCommonKSubstring(k, str1, str2):
+    if (k <= len(str1) and k <= len(str2) and k>= 1):
+        i = 0
+
+        while (i < len(str1) -k + 1):
+            found = isSubstring(str1[i:i+k-1])
+
+            if (found):
+                return True
+            i += 1
+    return False
+
+def maxCommonSubstringLength(str1, str2):
+    flag = True
+    size = len(str1)
+
+    while (size >= 0):
+        flag = haveCommonKSubstring(size, str1, str2)
+
+        if (flag):
+            return size
+        size -= 1
+    return 0
+
+
+print(isPrefix("str", "string"))
+print(isPrefix("string", "str"))
+print(isPrefix(" ", " "))
