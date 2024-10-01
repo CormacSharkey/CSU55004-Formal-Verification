0729b8c90a9535a96fa122bb5933d48d441743cf
CormacSharkey <csharkey@tcd.ie>
2024-10-01 15:26:25 +0100

    adding test cases

--- Group_Project_Part_1/module.py
+++ Group_Project_Part_1/module.py
@@ -1,6 +1,6 @@
 
 def isPrefix(pre, str):
-    if (len(pre) <= len(str)):
+    if (len(pre) <= len(str) and len(pre) >= 1 and len(str) >= 1):
         str_slice = str[0:len(pre)]
 
         if (pre == str_slice):
@@ -16,6 +16,7 @@ def isSubstring(sub, str):
             result = isPrefix(sub, str[i:])
             if (result):
                 return True
+            i += 1
     return False
 
 def haveCommonKSubstring(k, str1, str2):
@@ -23,7 +24,7 @@ def haveCommonKSubstring(k, str1, str2):
         i = 0
 
         while (i < len(str1) -k + 1):
-            found = isSubstring(str1[i:i+k-1])
+            found = isSubstring(str1[i:i+k-1], str2)
 
             if (found):
                 return True
@@ -45,4 +46,27 @@ def maxCommonSubstringLength(str1, str2):
 
 print(isPrefix("str", "string"))
 print(isPrefix("string", "str"))
-print(isPrefix(" ", " "))
+print(isPrefix("", ""))
+print("\n")
+
+
+print(isSubstring("str", "string"))
+print(isSubstring("str", "substring"))
+print(isSubstring("substring", "string"))
+print(isSubstring("", ""))
+print("\n")
+
+print(haveCommonKSubstring(3, "str", "string"))
+print(haveCommonKSubstring(5, "str", "string"))
+print(haveCommonKSubstring(5, "string", "substring"))
+print(haveCommonKSubstring(2, "", ""))
+print(haveCommonKSubstring(5, "substring", "string"))
+print(haveCommonKSubstring(3, "string", "str"))
+print("\n")
+
+print(maxCommonSubstringLength("string", "str"))
+print(maxCommonSubstringLength("substring", "string"))
+print(maxCommonSubstringLength("banana", "bananagun"))
+print(maxCommonSubstringLength("banana", "bananagun"))
+print(maxCommonSubstringLength("", ""))
+
